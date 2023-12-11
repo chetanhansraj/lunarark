36e32173869db04b49a13109dda4d960fcfbbc77
chetanhansraj <65060664+chetanhansraj@users.noreply.github.com>
2023-12-11 17:35:22 +0530

    Add files via upload

---
+++ d6d.js
@@ -0,0 +1,476 @@
+const newLocal = {
+    "name": "Lunar Ark Project",
+    "children": [
+        {
+    "name": "Mission Objective",
+    "children": [
+        { "name": "Lunar Habitat Design" },
+        { "name": "Architectural Design" },
+        { "name": "Environmental Considerations" },
+        { "name": "Interior Design" }
+    ]
+    },
+    {
+    "name": "Construction and Landing",
+    "children": [
+        { "name": "Construction Methods" },
+        { "name": "In-Situ Resource Utilization (ISRU)" },
+        { "name": "Radiation Shielding" },
+        { "name": "Temperature Control" },
+        { "name": "Micrometeorite Protection" },
+        { "name": "Regolith Protection" }
+    ]
+    },
+    {
+    "name": "Protection Against Lunar Environment",
+    "children": [
+        { "name": "Physical Shielding" },
+        { "name": "Active Shielding" },
+        { "name": "Insulation" },
+        { "name": "Heating and Cooling Systems" },
+        { "name": "Physical Barriers" },
+        { "name": "Regolith Management Systems" }
+    ]
+    },
+    {
+    "name": "Life Support and Living Conditions",
+    "children": [
+        { "name": "Cryogenic Storage" },
+        { "name": "Desiccation" },
+        { "name": "Temperature Control" },
+        { "name": "Humidity Control" },
+        { "name": "Atmosphere Control" },
+        { "name": "Condition Monitoring" }
+    ]
+    },
+    {
+    "name": "Materials and Construction Techniques",
+    "children": [
+        { "name": "Material Selection" },
+        { "name": "Material Development" },
+        { "name": "Material Durability" },
+        { "name": "Advanced Materials Science" },
+        { "name": "Use of Lunar Resources" },
+        { "name": "Radiation-Resistant Materials" },
+        { "name": "Temperature-Resistant Materials" },
+        { "name": "Regolith-Resistant Materials" },
+        { "name": "Material Testing and Validation" }
+    ]
+    },
+    {
+    "name": "Redundancy and Backup Systems",
+    "children": [
+        { "name": "Redundant Systems" },
+        { "name": "Backups" }
+    ]
+    },
+    {
+    "name": "Monitoring and Control Systems",
+    "children": [
+        { "name": "Condition Monitoring" },
+        { "name": "Retrieval Systems" }
+    ]
+    },
+    {
+    "name": "Retrieval Systems",
+    "children": [
+        { "name": "Robotics" }
+    ]
+    },
+    {
+    "name": "Exploration and Research Capabilities",
+    "children": [
+        { "name": "Preservation Systems" },
+        { "name": "Cryogenic Storage" },
+        { "name": "Desiccation" },
+        { "name": "Temperature Control" },
+        { "name": "Humidity Control" },
+        { "name": "Sealed Storage Containers" },
+        { "name": "Atmosphere Control" },
+        { "name": "Condition Monitoring" },
+        { "name": "Retrieval Systems" },
+        { "name": "Redundancy and Backup Systems" }
+    ]
+    },
+    {
+    "name": "Energy Generation and Storage",
+    "children": [
+        { "name": "Power Supply" },
+        { "name": "Energy Storage Systems" }
+    ]
+    },
+    {
+    "name": "Communication Systems",
+    "children": [
+        { "name": "Data Transmission" },
+        { "name": "Signal Reception" }
+    ]
+    },
+    {
+    "name": "Navigation and Positioning Systems",
+    "children": [
+        { "name": "GPS-like Systems" },
+        { "name": "Star Trackers" }
+    ]
+    },
+    {
+    "name": "Maintenance and Repair Systems",
+    "children": [
+        { "name": "Robotic Systems" },
+        { "name": "3D Printing Technologies" }
+    ]
+    },
+    {
+    "name": "Waste Management Systems",
+    "children": [
+        { "name": "Recycling Systems" },
+        { "name": "Waste-to-Energy Systems" }
+    ]
+    },
+    {
+    "name": "Safety and Emergency Systems",
+    "children": [
+        { "name": "Fire Suppression Systems" },
+        { "name": "Emergency Escape Systems" }
+    ]
+    },
+    {
+    "name": "Scientific Research Facilities",
+    "children": [
+        { "name": "Laboratories" },
+        { "name": "Telescopes" }
+    ]
+    },
+    {
+    "name": "Transportation Systems",
+    "children": [
+        { "name": "Lunar Rovers" },
+        { "name": "Spacecraft" }
+    ]
+    },
+    {
+    "name": "Legal and Ethical Considerations",
+    "children": [
+        { "name": "Compliance with International Space Law" },
+        { "name": "Ethical Issues Related to the Storage of Biological Materials" }
+    ]
+    },
+    {
+    "name": "Training and Human Factors",
+    "children": [
+        { "name": "Training Programs for Crew Members" },
+        { "name": "Consideration of Psychological Health and Social Dynamics" }
+    ]
+    },
+    {
+    "name": "Funding and Project Management",
+    "children": [
+        { "name": "Funding from Multiple Sources" },
+        { "name": "Project Management to Coordinate Different Aspects of the Project" }
+    ]
+    },
+    {
+    "name": "Public Engagement and Education",
+    "children": [
+        { "name": "Public Updates on the Project's Progress" },
+        { "name": "Educational Programs Related to the Project" }
+    ]
+    },
+    {
+    "name": "Site Selection",
+    "children": [
+        { "name": "Geographical Considerations" },
+        { "name": "Proximity to Resources" },
+        { "name": "Environmental Factors" }
+    ]
+    },
+    {
+    "name": "Lunar Environment Understanding",
+    "children": [
+        { "name": "Lunar Geology Studies" },
+        { "name": "Lunar Atmosphere Studies" },
+        { "name": "Lunar Climate Studies" }
+    ]
+    },
+    {
+    "name": "Space Weather Monitoring",
+    "children": [
+        { "name": "Solar Activity Monitoring" },
+        { "name": "Cosmic Radiation Monitoring" },
+        { "name": "Micrometeorite Monitoring" }
+    ]
+    },
+
+    {
+    "name": "Lunar Ark Design Evolution",
+    "children": [
+        { "name": "Design Improvements" },
+        { "name": "Technology Advancements" },
+        { "name": "Feedback Incorporation" }
+    ]
+    },
+    {
+    "name": "Lunar Ark Decommissioning Plan",
+    "children": [
+        { "name": "End-of-Life Plan for the Lunar Ark" },
+        { "name": "Plan for Minimizing Environmental Impact" }
+    ]
+    },
+    {
+    "name": "International Collaboration",
+    "children": [
+        { "name": "Collaboration with Other Countries or Organizations" },
+        { "name": "Sharing of Resources and Knowledge" }
+    ]
+    },
+    {
+    "name": "Contingency Planning",
+    "children": [
+        { "name": "Plan for Dealing with Unexpected Problems or Emergencies" },
+        { "name": "Backup Plans for Critical Systems" }
+    ]
+    },
+    {
+    "name": "Sustainability Considerations",
+    "children": [
+        { "name": "Minimizing Use of Non-Renewable Resources" },
+        { "name": "Maximizing Use of In-Situ Resources" },
+        { "name": "Minimizing Environmental Impact" }
+    ]
+    },
+    {
+    "name": "Technological Innovations",
+    "children": [
+        { "name": "Incorporation of New Technologies as They Become Available" },
+        { "name": "Development of New Technologies Specifically for the Lunar Ark" }
+    ]
+    },
+    {
+        "name": "Long-Term Vision and Goals",
+        "children": [
+            { "name": "Plan for the Future of the Lunar Ark" },
+            { "name": "Goals for Scientific Discovery, Preservation, and Exploration" }
+        ]
+        }
+    ]
+    };
+var treeData = newLocal;
+
+// Set the dimensions and margins of the diagram
+var diameter = 1920;
+var height = 1080;
+var margin = {top: 20, right: 120, bottom: 20, left: 120};
+
+// Append the svg object to the body of the page
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+  .append("g")
+    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+var i = 0,
+    duration = 750,
+    root;
+
+// Define the zoom function
+var zoom = d3.zoom()
+    .scaleExtent([0.1, 100]) // This defines the minimum and maximum zoom scale, change as needed
+    .on("zoom", function() {
+        svg.attr("transform", d3.event.transform);
+    });
+
+
+
+// Call the zoom function
+svg.call(zoom);
+
+// Declares a tree layout and assigns the size
+var tree = d3.cluster()
+    .size([360, diameter / 2 - 120]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
+  }
+}
+
+function update(source) {
+  // Assigns the x and y position for the nodes
+    var treeData = tree(root);
+
+  // Compute the new tree layout.
+    var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+  // Normalize for fixed-depth and to use polar coordinates instead of Cartesian
+    nodes.forEach(function(d) {
+    d.y = d.depth * 180;
+    d.x = (d.x * Math.PI) / 180;  // Convert from degrees to radians
+  });
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+    var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+    var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        return "rotate(" + (source.x0 - 90) + ") translate(" + source.y0 + ")";
+    })
+    .on('click', click);
+
+  // Add Circle for the nodes
+    nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  // Add labels for the nodes
+    nodeEnter.append('text')
+      .attr("dy", ".20em")
+      .attr("x", function(d) {
+          return d.x < Math.PI === !d.children ? 30 : -30;
+      })
+      .attr("text-anchor", function(d) {
+                   return d.x < Math.PI === !d.children ? "start" : "end";
+      })
+      .attr("transform", function(d) {
+          return d.x >= Math.PI ? "rotate(180)" : null;
+      })
+      .text(function(d) { return d.data.name; });
+
+  // UPDATE
+    var nodeUpdate = nodeEnter.merge(node);
+
+  // Transition to the proper position for the node
+    nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+      return "rotate(" + (d.x * 180 / Math.PI - 90) + ") translate(" + d.y + ")";
+    });
+
+  // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+    .attr('r', 15)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+    })
+    .attr('cursor', 'pointer');
+
+  // Remove any exiting nodes
+    var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "rotate(" + (source.x * 180 / Math.PI - 90) + ") translate(" + source.y + ")";
+        })
+        .remove();
+  
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+      .attr('r', 1e-6);
+  
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text')
+      .style('fill-opacity', 1e-6);
+
+
+    // Store the old positions for transition.
+    nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+    });
+
+
+    // ****************** links section ***************************
+
+    // Update the links...
+    var link = svg.selectAll('path.link')
+        .data(links, function(d) { return d.id; });
+
+    // Enter any new links at the parent's previous position.
+    var linkEnter = link.enter().insert('path', "g")
+    .attr("class", "link")
+    .attr('d', function(d){
+        return diagonal({source: d.parent, target: d}); 
+    });
+
+
+    // UPDATE
+    var linkUpdate = linkEnter.merge(link);
+    // Transition back to the parent element position
+    linkUpdate.transition()
+        .duration(duration)
+        .attr('d', function(d){ 
+            return diagonal({source: d.parent, target: d});
+        }); 
+
+    // Remove any exiting 
+
+    // EXIT - this removes old links
+    link.exit().remove();
+    
+    // var linkExit = link.exit().transition()
+    //     .duration(duration)
+    //     .attr('d', function(d) {
+    //     var o = {x: source.x, y: source.y}
+    //     return diagonal(o, o)
+    //     })
+    //     .remove();
+
+
+    // Creates a curved (diagonal) path from parent to the child nodes
+    function diagonal(d) {
+        return d3.linkRadial()
+            .angle(function(d) { return d.x; })
+            .radius(function(d) { return d.y; })
+            ({source: d.source, target: d.target});
+    }
+}
+    
+
+    
+var linkGenerator = d3.linkRadial()
+    .angle(function(d) { return d.x; })
+    .radius(function(d) { return d.y; });
+
+  // then use it like this:
+var pathData = linkGenerator({
+        source: sourceNode,
+        target: targetNode
+    });
+
+// Toggle children on click.
+function click(d) {
+    if (d.children) {
+    d._children = d.children;
+    d.children = null;
+    } else {
+    d.children = d._children;
+    d._children = null;
+    }
+    update(d);
+}
+
+var svg = d3.select("#d3-visualization").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .append("g")
+    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
